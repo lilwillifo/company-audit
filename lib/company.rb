1a4a9e4ac0eb13eb59d96e701a505954d9252744
lilwillifo <margaretwilliford4@gmail.com>
2018-02-27 10:24:15 -0700

    load employees in progress

--- lib/company.rb
+++ lib/company.rb
@@ -1,8 +1,18 @@
+require './lib/employee'
+require 'CSV'
+
 class Company
   attr_reader :employees, :projects, :timesheets
   def initialize
     @employees = []
     @projects = []
     @timesheets = []
+  end
+
+  def load_employees(filename)
+    CSV.foreach(filename) do |data|
+      @employees << Employee.new(data[0], data[1], data[2], data[3], data[4])
+    end
+    {success: true, error: nil}
   end
 end
--- test/company_test.rb
+++ test/company_test.rb
@@ -9,4 +9,24 @@ class CompanyTest < Minitest::Test
     assert_equal [], company.projects
     assert_equal [], company.timesheets
   end
+
+  def test_it_can_load_employees
+    company = Company.new
+    expected = {success: false, error: 'bad data'}
+
+    assert_equal expected, company.load_employees('./data/bad_employees.csv')
+
+    expected = {success: true, error: nil}
+    assert_equal expected, company.load_employees('./data/employees.csv')
+  end
 end
+
+#### company.load_employees(filename)
+
+# - Parameters: String
+# - Returns: Hash
+#
+# Read all data from `filename` in CSV format.
+#Only add its contents to `company.employees` if ALL data in the file is valid.
+#If any data is invalid, you should immediately return an error INSTEAD of saving the data.
+#A test file for bad employee data has been provided.
