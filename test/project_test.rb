6f2f94c407b9d93ef5ea160af7f34f4bf8e8d6ff
lilwillifo <margaretwilliford4@gmail.com>
2018-02-27 10:01:47 -0700

    timesheet class and test

--- test/project_test.rb
+++ test/project_test.rb
@@ -10,4 +10,17 @@ class ProjectTest < Minitest::Test
     project = Project.new(project_id, name, start_date, end_date)
     assert_instance_of Project, project
   end
+
+  def test_attributes
+    project_id = '123'
+    name = 'Widget Maker'
+    start_date = '2015-01-01'
+    end_date = '2018-01-01'
+    project = Project.new(project_id, name, start_date, end_date)
+
+    assert_equal 123, project.project_id
+    assert_equal 'Widget Maker', project.name
+    assert_instance_of Date, project.start_date
+    assert_instance_of Date, project.end_date
+  end
 end
