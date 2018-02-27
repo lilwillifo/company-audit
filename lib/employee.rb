9313f71f9644f1cfd4f7217759cb3851e9f20c14
lilwillifo <margaretwilliford4@gmail.com>
2018-02-27 09:44:08 -0700

    working on dates

--- lib/employee.rb
+++ lib/employee.rb
@@ -1,2 +1,17 @@
 class Employee
+  attr_reader :employee_id,
+              :name,
+              :role,
+              :start_date,
+              :end_date
+  def initialize(employee_id, name, role, start_date, end_date)
+    @employee_id = employee_id.to_i
+    @name = name
+    @role = role
+    start_year = start_date.split('-')[0].to_i
+    start_month = start_date.split('-')[1].to_i
+    start_day = start_date.split('-')[2].to_i
+    @start_date = Date.new(start_year, start_month, start_day)
+    # @end_date = Date.new(end_date)
+  end
 end
--- test/employee_test.rb
+++ test/employee_test.rb
@@ -3,9 +3,27 @@ require './lib/employee'
 
 class EmployeeTest < Minitest::Test
   def test_instantiation
-    employee = Employee.new
+    employee_id = '5'
+    name = 'Sally Jackson'
+    role = 'Engineer'
+    start_date = '2015-01-01'
+    end_date = '2018-01-01'
+    employee = Employee.new(employee_id, name, role, start_date, end_date)
   end
 
   def test_attributes
+    employee_id = '5'
+    name = 'Sally Jackson'
+    role = 'Engineer'
+    start_date = '2015-01-01'
+    end_date = '2018-01-01'
+    employee = Employee.new(employee_id, name, role, start_date, end_date)
+
+    assert_equal 5, employee.employee_id
+    assert_instance_of Integer, employee.employee_id
+    assert_equal 'Sally Jackson', employee.name
+    assert_equal 'Engineer', employee.role
+    assert_instance_of Date, employee.start_date
+    # assert_instance_of Date, employee.end_date
   end
 end
